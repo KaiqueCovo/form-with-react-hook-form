f429706d5fd287fa15e9ab512c2c1745ce466466
Kaique Covo <kaique.kng@gmail.com>
2021-12-18 18:58:54 -0300

    feat: Add react-hook-form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -4,12 +4,42 @@ import {
   Heading,
   Text,
   Button,
+  useToast
 } from '@chakra-ui/react';
+import { Controller, useForm } from 'react-hook-form';
 
 import { Input, Select } from '../'
 
-
 export function Form() {
+  const toast = useToast()
+
+  const {register, control, formState: { errors }, ...form } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      name: '',
+      lastName: '',
+      email: '',
+      cellphone: '',
+      job: ''
+    },
+  });
+
+  const handleSubmit = form.handleSubmit((values) => {
+    console.log(values)
+
+    if (!toast.isActive('contact-submitted')) {
+      toast({
+        id: 'contact-submitted',
+        title: 'Wow, Your contact is submitted!',
+        position: 'top-right',
+        variant: 'left-accent',
+        status: 'success',
+        isClosable: true,
+        containerStyle: { marginTop: 80 }
+      })
+    }
+  });
+
   return (
     <Stack
     bg={'gray.50'}
@@ -31,42 +61,54 @@ export function Form() {
         </Text>
       </Heading>
       <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
-        We’re looking for amazing engineers just like you! Become a part
-        of our rockstar engineering team and skyrocket your career!
+        We’re looking for amazing people just like you! Become a part of our rockstar team and skyrocket your career!
       </Text>
     </Stack>
-    <Box as={'form'} mt={10}>
+    <Box as={'form'} mt={10} onSubmit={handleSubmit}>
       <Stack spacing={4}>
         <Input
           name='name'
           type='text'
           placeholder='First name'
+          {...register('name') }
         />
         <Input
           name='lastName'
           type='text'
           placeholder='Last name'
+          {...register('lastName') }
         />
         <Input
           name="email"
           type="email"
           placeholder='Email'
+          {...register('email') }
         />
         <Input
-          name="phone"
+          name="cellphone"
           type="tel"
           placeholder='Cell phone'
+          {...register('cellphone') }
         />
-        <Select
-          placeholder="Jobs"
-          options={[
-            { label: 'Engenieer', value: 'engenieer' },
-            { label: 'Designer', value: 'designer' },
-            { label: 'People', value: 'people' },
-          ]}
-        />
+        <Controller
+          name='job'
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Select
+              placeholder="Jobs"
+              onChange={val => onChange( val.value )}
+              options={[
+                { label: 'Engenieer', value: 'engenieer' },
+                { label: 'Designer', value: 'designer' },
+                { label: 'People', value: 'people' },
+              ]}
+            />
+          )}
+        ></Controller>
+        
       </Stack>
       <Button
+      type='submit'
         fontFamily={'heading'}
         mt={8}
         w={'full'}
@@ -75,7 +117,8 @@ export function Form() {
         _hover={{
           bgGradient: 'linear(to-r, red.400,pink.400)',
           boxShadow: 'xl',
-        }}>
+        }} 
+      >
         Submit
       </Button>
     </Box>
